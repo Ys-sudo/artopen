1c30d4267fbadc29da32cc072c5f9604fff1b9c5
ys-sudo <georgios.lazaridis0@gmail.com>
2021-02-09 14:28:03 +0100

    Filmowanie + kalendarze firmowe

--- src/components/ModalForm/FilmoWanie/index.js
+++ src/components/ModalForm/FilmoWanie/index.js
@@ -11,10 +11,197 @@ class FilmoWanie extends React.Component {
   render() {
     return (
       <>
-      {/* Wystawiennictwo */}
+      {/* Flimy */}
 
       <input name='Filmowanie' id='filmowanie' className='subtitle' placeholder='Filmowanie:' disabled style={{color:'gray',fontFamily:'Poppins', backgroundColor:'white',border:'0px solid white'}} />
+      <div className='field' style={{marginLeft:'18px'}}>
 
+        <div className='columns'>
+
+        <div className='column' style={{marginRight:'10px'}} >
+
+          <label className='label'>Cel stworzenia filmu:</label>
+          <select
+          className='button-green select-green' as='select'  name='Cel stworzenia filmu' id='celfilmu' style={{minWidth:'300px'}}
+          onChange={this.handleChange}
+          >
+          <option value="Promocja produktu lub usługi">Promocja produktu lub usługi </option>
+          <option value="Promocja firmy">Promocja firmy </option>
+          <option value="Relacja z wydarzenia lub imprezy"> Relacja z wydarzenia lub imprezy</option>
+          <option value="Film szkoleniowy lub instruktażowy"> Film szkoleniowy lub instruktażowy</option>
+          <option value="Inne"> Inne</option>
+
+
+          </select>
+
+        </div>
+
+        <div className='column' >
+
+          <label className='label'>Użycie drona:</label>
+          <select
+          className='button-green select-green' as='select'  name='Dron' id='dron'
+          onChange={this.handleChange}
+          >
+          <option value="Tak">Tak</option>
+          <option value="Nie">Nie</option>
+
+
+          </select>
+
+        </div>
+
+        <div className='column' >
+
+          <label className='label'>Scenariusz:</label>
+          <select
+          className='button-green select-green' as='select'  name='Scenariusz' id='Scenariusz'
+          onChange={this.handleChange}
+          >
+          <option value="Własny">Własny</option>
+          <option value="Do zrealizowania">Do zrealizowania</option>
+
+
+          </select>
+
+        </div>
+
+
+      </div>
+
+      <div className='columns'>
+
+        <div className='column' >
+        <label className='label'> Opis filmu: </label>
+        <textarea  onChange={this.handleChange} className='textarea' type='text' rows='3'  placeholder='Opisz w kilku słowach film...' name='Opis filmu' id='opis filmu'  />
+        </div>
+
+        <div className='column' >
+        <label className='label'> Przykładowe realizacje: </label>
+        <textarea  onChange={this.handleChange} className='textarea' type='text' rows='3'  placeholder='Wpisz filmy które Ci się podobają...' name='Przykładowe realizacje' id='przykładowe filmy'  />
+
+
+        </div>
+
+
+
+      </div>
+
+
+
+      <div className='columns'>
+
+      <div className='column' style={{marginRight:'10px'}}>
+
+      <label className='label'>Długość filmu (min):</label>
+      <input className='input' onChange={this.handleChange} placeholder='45' type='number' name='Długość filmu' id='Długość filmu' />
+      <br /><br />
+
+      <label className='label'>Grupa docelowa:</label>
+      <input className='input' onChange={this.handleChange} placeholder='Dorośli 30+... ' type='text' name='grupa docelowa' id='grupa docelowaIII' />
+      <br /><br />
+      <label className='label'>Miejsce realizacji:</label>
+      <input className='input' onChange={this.handleChange} placeholder='Wrocław... ' type='text' name='Miejsce realizacji filmu' id='Miejsce realizacji filmu' />
+
+
+      </div>
+
+
+        <div className='column' >
+          <label  className='label is-size-10'>Dodatkowa forma przekazu:</label>
+          <br />
+
+        <label htmlFor="animacje2d" className='label main' style={{fontSize:'14px'}}>Animacje 2D
+          <input className='checkbox' type='checkbox' name='Animacje 2D' id='animacje2d'
+          onChange={this.handleChange} />
+          <span className="check"></span>
+          </label>
+
+          <label htmlFor="animacje3d" className='label main' style={{fontSize:'14px'}}>Animacje 3D
+            <input className='checkbox' type='checkbox' name='Animacje 3D' id='animacje3d'
+            onChange={this.handleChange} />
+            <span className="check"></span>
+            </label>
+
+            <label htmlFor="lektor" className='label main' style={{fontSize:'14px'}}>Lektor
+              <input className='checkbox' type='checkbox' name='Lektor' id='lektor'
+              onChange={this.handleChange} />
+              <span className="check"></span>
+              </label>
+
+              <label htmlFor="Dodatkowe wersje językowe" className='label main' style={{fontSize:'14px'}}>Dodatkowe wersje językowe
+                <input className='checkbox' type='checkbox' name='Dodatkowe wersje językowe' id='Dodatkowe wersje językowe'
+                onChange={this.handleChange} />
+                <span className="check"></span>
+                </label>
+
+        </div>
+
+        <div className='column' >
+
+        <label  className='label is-size-10'>Dystrybucja:</label>
+        <br />
+
+      <label htmlFor="TV" className='label main' style={{fontSize:'14px'}}>Telewizja
+        <input className='checkbox' type='checkbox' name='TV' id='TV'
+        onChange={this.handleChange} />
+        <span className="check"></span>
+        </label>
+
+        <label htmlFor="internet" className='label main' style={{fontSize:'14px'}}>Internet
+          <input className='checkbox' type='checkbox' name='internet' id='internet'
+          onChange={this.handleChange} />
+          <span className="check"></span>
+          </label>
+
+          <label htmlFor="innea" className='label main' style={{fontSize:'14px'}}>Inne (billboard, telebim itp.)
+            <input className='checkbox' type='checkbox' name='Inne' id='innea'
+            onChange={this.handleChange} />
+            <span className="check"></span>
+            </label>
+
+
+        </div>
+
+
+
+
+
+      </div>
+
+      <div className='columns'>
+
+
+        <div className='column'>
+        <label className='label'>Termin realizacji:</label>
+          <input className='button-green date' type='date' name='deadline' id='deadline'
+          onChange={this.handleChange}
+           />
+           </div>
+
+
+           <div className='column'>
+             <div className='control'>
+             <label className='label is-size-10'>Budżet:</label>
+             <input style={{width:'100%'}} min='1500' placeholder='25000' max='50000' id="rangea" className='range' name="budżet" type="range"
+             onInput={(event) => {console.log(document.getElementById('rangea').value);
+             document.getElementById('rvalueida').innerText = document.getElementById('rangea').value + ' zł';
+           }} onChange={this.handleChange}
+             />
+             <output style={{color:'#00d1b2',textAlign:'center'}} name="rvalueida" id="rvalueida">25000 zł</output>
+
+             </div>
+           </div>
+
+
+      </div>
+
+
+
+
+
+
+      </div>
       <hr />
 
 
--- src/components/ModalForm/KalFirm/index.js
+++ src/components/ModalForm/KalFirm/index.js
@@ -17,8 +17,207 @@ class KalFirm extends React.Component {
       <>
 
       <input name='Kalendarze firmowe' id='kalfirm' className='subtitle' placeholder='Kalendarze firmowe:' disabled style={{color:'gray',fontFamily:'Poppins', backgroundColor:'white',border:'0px solid white',marginBottom:'20px'}} />
+      <div className='field' style={{marginLeft:'18px'}}>
 
 
+
+
+      <div className='columns'>
+
+      <div className='control column'>
+
+      <label className='label'>Ilość:</label>
+
+        <input onChange={this.handleChange} style={{maxWidth:'200px'}} className='input' type='number' placeholder='100' name='Ilość kalendarzy' id='ilosck' />
+      </div>
+      <br />
+
+      <div className='column'>
+          <label className='label'>Typ kalendarza:</label>
+
+          <select
+          className='button-green select-green' as='select'  name='Typ kalendarza' id='typ'
+          onChange={this.handleChange}
+          onInput={(event) => {
+          console.log(event.target.value);
+            if((event.target.value == 'biurkowy') ||(event.target.value == 'planszowy')
+            || (event.target.value == 'listkowy')){
+              document.getElementById('glowka').style.display = 'none';
+            } else {
+              document.getElementById('glowka').style.display = 'block';
+            }
+          }}
+          >
+          <option value="jednodzielny">jednodzielny</option>
+          <option value="dwudzielny">dwudzielny</option>
+          <option value="trójdzielny">trójdzielny</option>
+          <option value="czterodzielny">czterodzielny</option>
+
+          <option value="biurkowy">biurkowy</option>
+          <option value="planszowy">planszowy</option>
+          <option value="listkowy">listkowy</option>
+
+          </select>
+
+
+          <br />
+        </div>
+        <div className='column' >
+          <div id='glowka'>
+          <label className='label'>Rodzaj główki:</label>
+          <select
+          className='button-green select-green' as='select'  name='Rodzaj główki' id='rodzaj głowki'
+          onChange={this.handleChange}
+          >
+          <option value="płaska">płaska</option>
+          <option value="wypukła">wypukła</option>
+          <option value="eco">eco</option>
+
+
+          </select>
+          </div>
+        </div>
+
+
+        </div>
+
+
+
+        <div className='columns'>
+          <div className='column'>
+            <label className='label'>Kalendarium:</label>
+            <select
+            className='button-green select-green' as='select'  name='Rodzaj główki' id='rodzaj głowki'
+            onChange={this.handleChange}
+            onInput={(event) => {
+            console.log(event.target.value);
+              if(event.target.value == 'indywidualne'){
+                document.getElementById('kalendariumI').style.display = 'block';
+              } else {
+                document.getElementById('kalendariumI').style.display = 'none';
+              }
+            }}
+            >
+            <option value="standardowe">standardowe</option>
+            <option value="indywidualne">indywidualne</option>
+            </select>
+          </div>
+
+          <div className='column'>
+            <label className='label'>Okienko:</label>
+            <select
+            className='button-green select-green' as='select'  name='Okienko' id='okienko'
+            onChange={this.handleChange}
+            >
+            <option value="standardowe">standardowe</option>
+            <option value="indywidualne">indywidualne</option>
+            </select>
+
+
+          </div>
+
+          <div className='column'>
+
+
+          <label className='label'>Projekt:</label>
+          <select
+          className='button-green select-green' as='select'  name='Projekt' id='proj'
+          onChange={this.handleChange}
+          >
+          <option value="Własny">Własny</option>
+          <option value="Zlecam wykonanie">Zlecam wykonanie</option>
+          </select>
+
+
+          </div>
+
+        </div>
+
+
+            <div id='kalendariumI' style={{display:'none'}}>
+            <label className='label'>Specyfikacja dni wolnych:</label>
+            <br />
+            <div className='control columns' id='dniwolne' style={{display:'flex',flexWrap:'wrap',width:'100%',marginLeft:'3%'}}>
+
+                <div className='column'>
+                  <label htmlFor="imieniny" className='label main' style={{fontSize:'14px'}}>Imieniny
+                  <input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Imieniny' id='imieniny' />
+                  <span className="check"></span>
+                  </label>
+                </div>
+                <div className='column'>
+                  <label htmlFor="swietaPL" className='label main' style={{fontSize:'14px'}}>Święta PL
+                  <input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Święta PL' id='swietaPL' />
+                  <span className="check"></span>
+                  </label>
+                </div>
+                <div className='column'>
+                  <label className='label main' htmlFor="swietaEU" style={{fontSize:'14px'}}>Święta EU
+                  <input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Święta EU' id='swietaEU' />
+                  <span className="check"></span>
+                  </label>
+                </div>
+                <div className='column'>
+                  <label className='label main' htmlFor="niestandarodwe" style={{fontSize:'14px'}}>Święta niestandardowe
+                  <input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Święta niestandardowe' id='niestandarodwe' />
+                  <span className="check"></span>
+                  </label>
+                </div>
+
+            </div>
+          </div>
+
+        <label className='label'>Uszlachetnienia:</label>
+        <br />
+        <div className='control columns' id='UszlachetnieniaIII' style={{display:'flex',flexWrap:'wrap',width:'100%',marginLeft:'3%'}}>
+          <div className='column'>
+              <label htmlFor="HotstamplingII" className='label main' style={{fontSize:'14px'}}>Hotstampling
+              <input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Hotstampling' id='HotstamplingII' />
+              <span className="check"></span>
+              </label>
+            </div>
+            <div className='column'>
+              <label className='label main' htmlFor="lakieruvII" style={{fontSize:'14px'}}>Lakier UV<input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Lakier UV' id='lakieruvII' />
+              <span className="check"></span>
+              </label>
+            </div>
+            <div className='column'>
+              <label htmlFor="folia blyskII" className='label main' style={{fontSize:'14px'}}>Folia błysk<input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Folia błysk' id='folia blyskII' />
+              <span className="check"></span>
+              </label>
+            </div>
+            <div className='column'>
+              <label htmlFor="folia matII" className='label main' style={{fontSize:'14px'}}>Folia mat<input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Folia mat' id='folia matII' />
+              <span className="check"></span>
+              </label>
+            </div>
+            <div className='column'>
+              <label className='label main' htmlFor="foliasoftII" style={{fontSize:'14px'}}>Folia soft touch<input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Folia soft touch' id='foliasoftII' />
+              <span className="check"></span>
+              </label>
+            </div>
+            <div className='column'>
+              <label className='label main' htmlFor="ecopapierII" style={{fontSize:'14px'}}>Eco papier<input className='checkbox' onChange={this.handleChange} type='checkbox'  name='Eco Papier' id='ecopapierII' />
+              <span className="check"></span>
+              </label>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
           <hr />
 
 
